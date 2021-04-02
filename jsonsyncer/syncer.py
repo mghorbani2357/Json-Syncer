9fc1201a9b0af89f7471f5a4e4e420d7a74d7ac9
Mohsen Ghorbani <m.ghorbani2357@gmail.com>
2021-04-02 14:03:41 +0430

    Json file syncs with json variable

--- jsonsyncer/syncer.py
+++ jsonsyncer/syncer.py
@@ -4,13 +4,14 @@ from functools import wraps
 from os import path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import os
 
 
 def dump_json(func):
     @wraps(func)
     def wrapper(self, *args, **kw):
         result = func(self, *args, **kw)
-        self.__ignore_changes = True
+        # print('dump', self.__ignore_changes)
         with open(self.json_file_path, 'w') as json_file:
             json.dump(self.store, json_file)
         return result
@@ -39,11 +40,14 @@ class JsonSyncer(MutableMapping):
         with open(self.json_file_path, 'w') as json_file:
             json.dump(self.store, json_file)
 
+        self.stat = os.stat(self.json_file_path)
+
         self.__observer.schedule(self.__event_handler, path=json_file_path, recursive=False)
         self.__observer.start()
 
     @dump_json
     def __setitem__(self, key, value):
+        self.__ignore_changes = True
         self.store[key] = value
 
     @dump_json
@@ -51,13 +55,15 @@ class JsonSyncer(MutableMapping):
         del self.store[key]
 
     def __load_json(self):
-        with open(self.json_file_path, 'w') as json_file:
-            json.dump(self.store, json_file)
+        with open(self.json_file_path) as json_file:
+            self.store = json.load(json_file)
 
     def __on_modified(self, event):
-        if not self.__ignore_changes:
-            self.__load_json()
-        self.__ignore_changes = False
+        if event.src_path == self.json_file_path and os.stat(self.json_file_path).st_ctime > self.stat.st_ctime:
+            self.stat = os.stat(self.json_file_path)
+            if not self.__ignore_changes:
+                self.__load_json()
+            self.__ignore_changes = False
 
     def __getitem__(self, key):
         return self.store[key]
@@ -66,7 +72,7 @@ class JsonSyncer(MutableMapping):
         return iter(self.store)
 
     def __len__(self):
-        return len(self.store)
+        return self.store.__len__()
 
     def __repr__(self):
         return str(self.store)
